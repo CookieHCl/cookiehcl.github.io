c25677059ee1767a6576d25d289a8633027d1293
CookieHCl <nhc7502@snu.ac.kr>
2023-10-23 00:33:40 +0900

    [HEXO] Fix abbrlink to really work

--- scripts/abbrlink.js
+++ scripts/abbrlink.js
@@ -1,31 +1,39 @@
 let abbrlinks = {};
 
 hexo.extend.filter.register('before_generate', () => {
-    let posts = hexo.model('Post')
-    let undefined_posts = []
-    let max_abbrlink = 0
+    const posts = hexo.locals.get('posts').data;
+    let undefined_posts = [];
+    let max_abbrlink = 0;
 
-    posts.forEach(post => {
+    // get max abbrlink
+    for (const post of posts) {
         if (post.abbrlink) {
             abbrlinks[post._id] = post.abbrlink;
             max_abbrlink = Math.max(max_abbrlink, post.abbrlink);
         }
         else {
-            undefined_posts.push(post)
+            undefined_posts.push(post);
         }
-    });
+    }
 
-    undefined_posts.sort((a, b) => {
-        return a.date - b.date;
-    });
-    undefined_posts.forEach(post => {
+    // generate abbrlink for undefined posts
+    undefined_posts.sort((a, b) => a.date - b.date);
+    for (const post of undefined_posts) {
         abbrlinks[post._id] = ++max_abbrlink;
-    });
+    }
 });
 
 hexo.extend.filter.register('post_permalink', (data) => {
-    if (!data.abbrlink) {
-        data.abbrlink = abbrlinks[data._id]
+    // if abbrlink is already set, do nothing
+    if (data.abbrlink) {
+        return data;
     }
+
+    // set abbrlink if abbrlink is generated
+    const abbrlink = abbrlinks[data._id];
+    if (abbrlink) {
+        data.abbrlink = abbrlink;
+    }
+
     return data;
 }, 1);
