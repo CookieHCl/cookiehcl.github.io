a2f5f2bc45510b7b2390e832f356619664104446
CookieHCl <nhc7502@snu.ac.kr>
2025-03-14 11:02:07 +0900

    [REPO] Add new script for create new post task

---
+++ create_new_post.js
@@ -0,0 +1,28 @@
+const { execSync } = require('child_process');
+
+function toSnakeCase(str) {
+	return str.toLowerCase().replace(/\s+/g, '_');
+}
+
+function getDateStr() {
+	const today = new Date();
+	const year = String(today.getFullYear()).slice(-2);
+	const month = String(today.getMonth() + 1).padStart(2, '0');
+	const day = String(today.getDate()).padStart(2, '0');
+	return `${year}${month}${day}`;
+}
+
+if (process.argv.length != 4) {
+	console.error("Usage: node script.js <dir> <title>");
+	process.exit(1);
+}
+
+const dir = process.argv[2];
+const title = process.argv[3];
+
+const filename = `${getDateStr()}-${toSnakeCase(title)}`;
+const filePath = `${dir}/${filename}`;
+
+const command = `npx hexo new --path "${filePath}" "${title}" && rmdir "source/_posts/${filePath}" && code "source/_posts/${filePath}.md"`;
+const output = execSync(command, { encoding: 'utf-8' });
+console.log(output);
